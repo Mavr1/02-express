5ac661ee8009e078dc520ba362179190b2bb0b12
Mavr1 <mavrodiy.m.e@gmail.com>
2020-10-17 12:57:38 +0300

    fixed

--- src/contacts/contacts.controller.js
+++ src/contacts/contacts.controller.js
@@ -5,11 +5,11 @@ exports.getContacts = async (req, res, next) => {
   const [error, contacts] = await promiseHandler(contactsModel.listContacts());
   const parsedContacts = JSON.parse(contacts);
 
-  res.status(200).json(parsedContacts);
-
   if (error) {
     next(error);
   }
+
+  res.status(200).json(parsedContacts);
 };
 
 exports.getContact = async (req, res, next) => {
@@ -22,22 +22,24 @@ exports.getContact = async (req, res, next) => {
     res.status(404).json({ message: 'Contact not found' });
     return;
   }
-  res.status(200).json(contact);
 
   if (error) {
     next(error);
   }
+
+  res.status(200).json(contact);
 };
 
 exports.addContact = async (req, res, next) => {
   const [error, newContact] = await promiseHandler(
     contactsModel.addContact(req.body)
   );
-  res.status(201).json(newContact);
 
   if (error) {
     next(error);
   }
+
+  res.status(201).json(newContact);
 };
 
 exports.removeContact = async (req, res, next) => {
@@ -53,11 +55,11 @@ exports.removeContact = async (req, res, next) => {
 
   contactsModel.removeContact(contactId);
 
-  res.status(200).json({ message: 'Contact deleted' });
-
   if (error) {
     next(error);
   }
+
+  res.status(200).json({ message: 'Contact deleted' });
 };
 
 exports.updateContact = async (req, res, next) => {
@@ -75,9 +77,9 @@ exports.updateContact = async (req, res, next) => {
     contactsModel.updateContact(contactId, req.body)
   );
 
-  res.status(200).json(updatedContact);
-
   if (errorContact || errorUpdate) {
     next(errorContact || errorUpdate);
   }
+
+  res.status(200).json(updatedContact);
 };
